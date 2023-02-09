4e6561d260dd60eb3b4db8d72833e2f55b22d99f
SSS_Krut <m_troc@mail.ru>
2023-02-09 06:10:37 +0300

    Revert "Added parser using thread; started factoring in commands"
    This reverts commit 27578b37fbf137d7ed45ea16f9c97989396f7943.

--- src/commandLine/Commands.java
+++
@@ -1,8 +0,0 @@
-package commandLine;
-
-import java.util.ArrayList;
-
-public class Commands {
-    static ArrayList<String>commands=new ArrayList<String>();
-    //Commands(String semantic, )
-}
--- src/exceptions/ParseError.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class ParseError extends Exception{
-    ParseError(String message){super(message);}
-}

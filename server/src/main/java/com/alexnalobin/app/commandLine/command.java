9f15ad3eeb4ee39dc739bb5109fdc357dbd75a16
Alexander Nalobin <m_troc@mail.ru>
2023-07-30 20:11:06 +0300

    Delete all Server's part

--- server/src/main/java/com/alexnalobin/app/commandLine/command.java
+++
@@ -1,7 +0,0 @@
-package com.alexnalobin.app.commandLine;
-
-public interface command {
-    void execute() throws InterruptedException;
-    void repeat();
-    void set_next_command(command com);
-}
